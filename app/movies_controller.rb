57a312b80b7fb121b855567fafa1646c67b58a16
Megan Comeau <mlcomeau99@gmail.com>
2020-05-18 23:39:11 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,8 +76,9 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.find_by(title: "Wat?")
+  movie.update(title: "Wat, huh?")
+
 end
 
 def can_update_multiple_items_at_once
